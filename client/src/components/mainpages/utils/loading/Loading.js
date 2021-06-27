7816ebf2a55594227d252fa19894578bd38c855a
yash-rajpurohit <65324126+yash-rajpurohit@users.noreply.github.com>
2021-06-27 09:39:36 +0530

    Add files via upload

---
+++ client/src/components/mainpages/utils/loading/Loading.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './loading.css'
+
+function Loading() {
+    return (
+        // <div className="load-page">
+        //     <div className="loader">
+        //         <div>
+        //             <div>
+        //                 <div>
+        //                     <div>
+        //                         <div>
+        //                             <div>
+        //                                 <div>
+        //                                     <div></div>
+        //                                 </div>
+        //                             </div>
+        //                         </div>
+        //                     </div>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+
+
+        <div class="preloader loading">
+  <span class="slice"></span>
+  <span class="slice"></span>
+  <span class="slice"></span>
+  <span class="slice"></span>
+  <span class="slice"></span>
+  <span class="slice"></span>
+</div>
+    )
+}
+
+export default Loading
